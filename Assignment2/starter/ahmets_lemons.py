5100dfa89e9f4c6ac90b8fe9666a14c82a19b540
Ahmet Uysal <auysal16@ku.edu.tr>
2020-04-28 19:03:49 +0300

    Add assignment 2

---
+++ Assignment2/starter/ahmets_lemons.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+
+# use pandas to read csv data.
+# select column you want to use with lemon_data['column_name']
+
+# create your figure here
+
+plt.show()
